4cfab79f064de15214a56c9e13a7b6fd329628a3
Romain Beucher <rbeucher@thermo-geology.fr>
2019-10-15 18:02:55 +1100

    up to v0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyRadialPlot',
-      version='0.3',
+      version='0.4',
       description='Radial Plot using python and matplotlib',
       url='http://github.com/rbeucher/pyRadialPlot',
       author='Romain Beucher',
